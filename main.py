f18c23b9bd3c9ef597d3228f30a08b23513b4ee8
Isaac Ismail <Isaac Ismail>
2025-09-08 20:01:40 +1000

    fixing linking

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app = Flask(__name__, template_folder='templates_1')
 def home():
     return render_template('home_page.html')
 
+@app.route('/friends_list')
+def friends_list():
+    return render_template('friends_list.html')
+
+@app.route('/settings_page')
+def settings_page():
+    return render_template('settings_page.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
