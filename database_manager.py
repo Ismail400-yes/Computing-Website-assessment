7bf6a5d580664d4acf4111f1160dca97bb43c331
Isaac Ismail <Isaac Ismail>
2025-10-01 20:07:08 +1000

    fix

--- database_manager.py
+++ database_manager.py
@@ -3,6 +3,6 @@ import sqlite3 as sql
 def listExtension():
   con = sql.connect("database/data_source.db")
   cur = con.cursor()
-  data = cur.execute("SELECT Chat_Image_Link, Title FROM conversation_table").fetchall()
+  data = cur.execute("SELECT Chat_Image_Link, Title FROM conversation_table WHERE Chat_Image_Link IS NOT NULL AND Chat_Image_Link != ''").fetchall()
   con.close()
   return data
