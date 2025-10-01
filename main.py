36660bfc71cc0d25f9d5a604d823cf96e2935bf1
Isaac Ismail <Isaac Ismail>
2025-10-01 19:56:30 +1000

    fix

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sqlite3 as sql
 from flask import Flask, render_template
 from flask import request
 import database_manager
@@ -5,8 +6,9 @@ import database_manager
 app = Flask(__name__, template_folder='templates_1')
 
 @app.route('/')
-def home():
-    return render_template('home_page.html')
+def index():
+    data = database_manager.listExtension()
+    return render_template('home_page.html', chat_pfp=data)
 
 @app.route('/friends_list')
 def friends_list():
@@ -20,12 +22,5 @@ def settings_page():
 def login_page():
     return render_template('login_page.html')
 
-@app.route('/home_page.html', methods=['GET'])
-@app.route('/', methods=['POST', 'GET'])
-
-def index():
-   data = database_manager.listExtension()
-   return render_template('/home_page.html', chat_pfp=data)
-
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
